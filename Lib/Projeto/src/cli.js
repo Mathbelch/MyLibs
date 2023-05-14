95a0bbd47c240f306819372a560b601fe13fd0fd
Matheus <matheusbelchior23@gmail.com>
2023-05-14 01:09:43 -0300

    Inserção função de validação dos links

--- Lib/Projeto/src/cli.js
+++ Lib/Projeto/src/cli.js
@@ -1,6 +1,7 @@
 import pegaArquivo from "./index.js";
 import chalk from 'chalk';
 import fs from 'fs';
+import listaValidada from "./http-validacao.js";
 
 //Usano o objeto process, próprio do node, que permite obter o que foi inserido na linha de comando de chamada do código (argv - argumentos da linha de comando): 
 const caminho = process.argv;
@@ -11,6 +12,7 @@ const caminho = process.argv;
 // Vamos usar a biblioteca fs. A função lstatSync() permite iteragir com o argumento específicado. Caso o argumento passado na linha de comando seja um caminho de arquivo  (isFile() = true) iremos chamar a função pegaArquivo direto com o caminho do arquivo; Caso o argumento seja uma pasta (isDirectory() = true), vamos usar o readdir() que retorna um array com os nomes dos arquivos na pasta.
 async function processaConteudo(argumentos) {
    const caminho = argumentos[2];
+   const valida = argumentos[3] === '--valida';
 
    try {
       fs.lstatSync(caminho);
@@ -23,20 +25,27 @@ async function processaConteudo(argumentos) {
 
    if (fs.lstatSync(caminho).isFile()) {
       const resultado = await pegaArquivo(caminho);
-      imprimeLista(resultado);
+      imprimeLista(valida, resultado);
    } else if (fs.lstatSync(caminho).isDirectory()) {
       const arquivos = await fs.promises.readdir(caminho);
       arquivos.forEach(async (nomeDeArquivo) => {
          const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
-         imprimeLista(lista, nomeDeArquivo);
+         imprimeLista(valida, lista, nomeDeArquivo);
       })
    }
 
-   function imprimeLista(resultado, identificador = '') {
+   async function imprimeLista(valida, resultado, identificador = '') {
+      if (valida) {
+         console.log(
+            chalk.yellow('Lista Validada: '), 
+            chalk.black.bgGreen(identificador),
+            await listaValidada(resultado));
+      } else {
       console.log(
          chalk.yellow('Listas de Links: '), 
          chalk.black.bgGreen(identificador),
          resultado);
+      }
    }
 
    
