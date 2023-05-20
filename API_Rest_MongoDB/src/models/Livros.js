cfc218ffb373a5b7571e35d77c8e438976867ff0
Matheus <matheusbelchior23@gmail.com>
2023-05-20 15:50:38 -0300

    inserção de métodos CRUD para entidade Livros

--- API_Rest_MongoDB/src/models/Livros.js
+++ API_Rest_MongoDB/src/models/Livros.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema(
    {
-      id: {type: String, required: true},
+      id: {type: String},
       titulo: {type: String, required: true},
       autor: {type: String, required: true},
+      editora: {type: String, required: true},
       numeroPaginas: {type: Number}
    }
 );
