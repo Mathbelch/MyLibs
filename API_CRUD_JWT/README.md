d583d08785268af7f12c944930a53b5673468f41
Matheus <matheusbelchior23@gmail.com>
2023-05-30 20:10:56 -0300

    excluindo API com JWT

--- API_CRUD_JWT/README.md
+++
@@ -1,2 +0,0 @@
-# Blog do código
-> API de blog simples em Node.js, com foco em requisitos de segurança.
--- API_CRUD_JWT/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-const { estrategiasAutenticacao } = require('./src/usuarios/estrategias-autenticacao');
-
-app.use(bodyParser.json());
-
-module.exports = app;
