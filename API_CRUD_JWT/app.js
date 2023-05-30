4edc18fff771185f100bc195177aa587dd581c34
Matheus <matheusbelchior23@gmail.com>
2023-05-29 21:29:58 -0300

    iniciando estratégia de autenticação de login com JWT

--- API_CRUD_JWT/app.js
+++ API_CRUD_JWT/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const { estrategiasAutenticacao } = require('./src/usuarios/estrategias-autenticacao');
+
 app.use(bodyParser.json());
 
 module.exports = app;
