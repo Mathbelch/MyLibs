28794821f0cba4d25ed28901a201fde2379b4cc4
Matheus <matheusbelchior23@gmail.com>
2023-06-01 23:17:28 -0300

    incluindo arquivos TypeScript em app/

--- TS_Negociacoes/app/app.ts
+++ TS_Negociacoes/app/app.ts
@@ -1,7 +1,8 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
+
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
 form.addEventListener('submit', event => {
-    event.preventDefault(); // Para o submit não recarregar a página.
-    controller.adiciona();
-});
+   event.preventDefault(); // Para o submit não recarregar a página.
+   controller.adiciona();
+})
