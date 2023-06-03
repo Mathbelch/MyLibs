589b5010697d5280989d7c429e919bcdc78d15ac
Matheus <matheusbelchior23@gmail.com>
2023-06-02 21:59:31 -0300

    incluindo modelo de view para renderizar tabela com dados de negociações

--- TS_Negociacoes/app/app.ts
+++ TS_Negociacoes/app/app.ts
@@ -1,8 +1,10 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
+import { NegociacoesView } from "./views/negociacoes-views.js";
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
 form.addEventListener('submit', event => {
    event.preventDefault(); // Para o submit não recarregar a página.
    controller.adiciona();
-})
+});
+
