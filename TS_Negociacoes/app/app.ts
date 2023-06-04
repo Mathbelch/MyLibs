dc2ce95a82b90e8368139389116055ee1af11e90
Matheus <matheusbelchior23@gmail.com>
2023-06-04 19:59:32 -0300

    refatorando o código: adicionando removeComments: true em tsconfig.json para não levar comentários nos arquivos .ts para os arquivos .js; ativando strictNullChecks para garantir integridado de dados de consultas a APIs

--- TS_Negociacoes/app/app.ts
+++ TS_Negociacoes/app/app.ts
@@ -1,10 +1,14 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
-import { NegociacoesView } from "./views/negociacoes-views.js";
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-   event.preventDefault(); // Para o submit não recarregar a página.
-   controller.adiciona();
-});
+if (form){
+   form.addEventListener('submit', event => {
+      event.preventDefault(); // Para o submit não recarregar a página.
+      controller.adiciona();
+   });
+} else {
+   throw Error('Não foi possível inicializar aplicação. Verifique a existência do form.')
+};
+
 
