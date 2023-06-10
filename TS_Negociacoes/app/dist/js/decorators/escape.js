96d8a1753421f9f28f13ce16169fdfda44156884
Matheus <matheusbelchior23@gmail.com>
2023-06-10 12:58:16 -0300

    refatorando códigos para acesso dos elementos HTML na instanciação de NeogiacaoController para isola-los no decorator dom-injector

--- TS_Negociacoes/app/dist/js/decorators/escape.js
+++ TS_Negociacoes/app/dist/js/decorators/escape.js
@@ -3,8 +3,6 @@ export function escape(target, propertyKey, descriptor) {
     descriptor.value = function (...args) {
         let retorno = metodoOriginal.apply(this, args);
         if (typeof retorno === 'string') {
-            console.log(`@escape ativado na classe ${this.constructor.name} para o método ${propertyKey}`);
-            retorno = retorno.replace(/<script>[\s\S]*?<\script>/, '');
         }
         ;
         return retorno;
