124655039542a5fa5b2ffbc28552a05f881ea9e7
Matheus <matheusbelchior23@gmail.com>
2023-06-10 12:02:15 -0300

    ajustando console.log

--- TS_Negociacoes/app/src/decorators/inspect.ts
+++ TS_Negociacoes/app/src/decorators/inspect.ts
@@ -6,8 +6,8 @@ export function inspect(
 ) {
    const metodoOriginal = descriptor.value;
    descriptor.value = function(...args: any[]){
-      console.log(`-- Método ${propertyKey}`);
-      console.log(`------ Parâmetroe: ${JSON.stringify(args)}`);
+      console.log(`## Método ${propertyKey}`);
+      console.log(`------ Parâmetros: ${JSON.stringify(args)}`);
       const retorno = metodoOriginal.apply(this, args);
       console.log(`------ Retorno: ${JSON.stringify(retorno)}`);
       return retorno;
